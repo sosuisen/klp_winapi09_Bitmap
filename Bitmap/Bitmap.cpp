092af92aee14694d098cb91b6ff69cdfd06d4ecc
sosuisen <hidekazu.kubota@gmail.com>
2022-11-30 17:11:34 +0900

    fix: add bekkai

--- Bitmap/Bitmap.cpp
+++ Bitmap/Bitmap.cpp
@@ -1,8 +1,9 @@
 ﻿#include <windows.h>
 #include <string>
+#include <chrono>
 
-static const int MAX_WIDTH = 1000;
-static const int MAX_HEIGHT = 500;
+static const int WIN_WIDTH = 1000;
+static const int WIN_HEIGHT = 500;
 
 LRESULT CALLBACK WndProc(
     HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -35,7 +36,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         szAppName, szAppName,
         WS_OVERLAPPEDWINDOW,
         50, 50,
-        MAX_WIDTH, MAX_HEIGHT,
+        WIN_WIDTH, WIN_HEIGHT,
         NULL, NULL,
         hInstance, NULL);
 
@@ -52,138 +53,51 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return (int)msg.wParam;
 }
 
-COLORREF avr3x3Filter(HDC hMemDC, int x, int y) {
-    COLORREF c00 = GetPixel(hMemDC, x - 1, y - 1);
-    COLORREF c01 = GetPixel(hMemDC, x - 1, y);
-    COLORREF c02 = GetPixel(hMemDC, x - 1, y + 1);
-    COLORREF c10 = GetPixel(hMemDC, x, y - 1);
-    COLORREF c11 = GetPixel(hMemDC, x, y);
-    COLORREF c12 = GetPixel(hMemDC, x, y + 1);
-    COLORREF c20 = GetPixel(hMemDC, x + 1, y - 1);
-    COLORREF c21 = GetPixel(hMemDC, x + 1, y);
-    COLORREF c22 = GetPixel(hMemDC, x + 1, y + 1);
-
-    byte r = (GetRValue(c00) + GetRValue(c01) + GetRValue(c02)
-        + GetRValue(c10) + GetRValue(c11) + GetRValue(c12)
-        + GetRValue(c20) + GetRValue(c21) + GetRValue(c22)) / 9;
-    byte g = (GetGValue(c00) + GetGValue(c01) + GetGValue(c02)
-        + GetGValue(c10) + GetGValue(c11) + GetGValue(c12)
-        + GetGValue(c20) + GetGValue(c21) + GetGValue(c22)) / 9;
-    byte b = (GetBValue(c00) + GetBValue(c01) + GetBValue(c02)
-        + GetBValue(c10) + GetBValue(c11) + GetBValue(c12)
-        + GetBValue(c20) + GetBValue(c21) + GetBValue(c22)) / 9;
-    return RGB(r, g, b);
+DWORD mosaic5x5(int shift, LPDWORD src, int index, int width) {
+    return ((src[index] >> shift & 0xff)
+        + (src[index + 1] >> shift & 0xff)
+        + (src[index + 2] >> shift & 0xff)
+        + (src[index + 3] >> shift & 0xff)
+        + (src[index + 4] >> shift & 0xff)
+        + (src[index + width] >> shift & 0xff)
+        + (src[index + 1 + width] >> shift & 0xff)
+        + (src[index + 2 + width] >> shift & 0xff)
+        + (src[index + 3 + width] >> shift & 0xff)
+        + (src[index + 4 + width] >> shift & 0xff)
+        + (src[index + width * 2] >> shift & 0xff)
+        + (src[index + 1 + width * 2] >> shift & 0xff)
+        + (src[index + 2 + width * 2] >> shift & 0xff)
+        + (src[index + 3 + width * 2] >> shift & 0xff)
+        + (src[index + 4 + width * 2] >> shift & 0xff)
+        + (src[index + width * 3] >> shift & 0xff)
+        + (src[index + 1 + width * 3] >> shift & 0xff)
+        + (src[index + 2 + width * 3] >> shift & 0xff)
+        + (src[index + 3 + width * 3] >> shift & 0xff)
+        + (src[index + 4 + width * 3] >> shift & 0xff)
+        + (src[index + width * 4] >> shift & 0xff)
+        + (src[index + 1 + width * 4] >> shift & 0xff)
+        + (src[index + 2 + width * 4] >> shift & 0xff)
+        + (src[index + 3 + width * 4] >> shift & 0xff)
+        + (src[index + 4 + width * 4] >> shift & 0xff))/25;
 }
 
+void mosaic5x5Filter(LPDWORD src, LPDWORD dst, RECT rect, int index, int width) {
+    int orgX = index % width;
+    int orgY = index / width;
 
-COLORREF avr5x5Filter(HDC hMemDC, int x, int y) {
-    COLORREF c00 = GetPixel(hMemDC, x - 2, y - 2);
-    COLORREF c01 = GetPixel(hMemDC, x - 2, y - 1);
-    COLORREF c02 = GetPixel(hMemDC, x - 2, y);
-    COLORREF c03 = GetPixel(hMemDC, x - 2, y + 1);
-    COLORREF c04 = GetPixel(hMemDC, x - 2, y + 2);
-    COLORREF c10 = GetPixel(hMemDC, x - 1, y - 2);
-    COLORREF c11 = GetPixel(hMemDC, x - 1, y - 1);
-    COLORREF c12 = GetPixel(hMemDC, x - 1, y);
-    COLORREF c13 = GetPixel(hMemDC, x - 1, y + 1);
-    COLORREF c14 = GetPixel(hMemDC, x - 1, y + 2);
-    COLORREF c20 = GetPixel(hMemDC, x, y - 2);
-    COLORREF c21 = GetPixel(hMemDC, x, y - 1);
-    COLORREF c22 = GetPixel(hMemDC, x, y);
-    COLORREF c23 = GetPixel(hMemDC, x, y + 1);
-    COLORREF c24 = GetPixel(hMemDC, x, y + 2);
-    COLORREF c30 = GetPixel(hMemDC, x + 1, y - 2);
-    COLORREF c31 = GetPixel(hMemDC, x + 1, y - 1);
-    COLORREF c32 = GetPixel(hMemDC, x + 1, y);
-    COLORREF c33 = GetPixel(hMemDC, x + 1, y + 1);
-    COLORREF c34 = GetPixel(hMemDC, x + 1, y + 2);
-    COLORREF c40 = GetPixel(hMemDC, x + 2, y - 2);
-    COLORREF c41 = GetPixel(hMemDC, x + 2, y - 1);
-    COLORREF c42 = GetPixel(hMemDC, x + 2, y);
-    COLORREF c43 = GetPixel(hMemDC, x + 2, y + 1);
-    COLORREF c44 = GetPixel(hMemDC, x + 2, y + 2);
+    if (orgX < rect.left || orgX > rect.right || orgY < rect.top || orgY > rect.bottom) {
+        dst[index] = src[index];
+        return;
+    }
 
-    byte r = (GetRValue(c00) + GetRValue(c01) + GetRValue(c02) + GetRValue(c03) + GetRValue(c04)
-        + GetRValue(c10) + GetRValue(c11) + GetRValue(c12) + GetRValue(c13) + GetRValue(c14)
-        + GetRValue(c20) + GetRValue(c21) + GetRValue(c22) + GetRValue(c23) + GetRValue(c24)
-        + GetRValue(c30) + GetRValue(c31) + GetRValue(c32) + GetRValue(c33) + GetRValue(c34)
-        + GetRValue(c40) + GetRValue(c41) + GetRValue(c42) + GetRValue(c43) + GetRValue(c44)
-        ) / 25;
-    byte g = (GetGValue(c00) + GetGValue(c01) + GetGValue(c02) + GetGValue(c03) + GetGValue(c04)
-        + GetGValue(c10) + GetGValue(c11) + GetGValue(c12) + GetGValue(c13) + GetGValue(c14)
-        + GetGValue(c20) + GetGValue(c21) + GetGValue(c22) + GetGValue(c23) + GetGValue(c24)
-        + GetGValue(c30) + GetGValue(c31) + GetGValue(c32) + GetGValue(c33) + GetGValue(c34)
-        + GetGValue(c40) + GetGValue(c41) + GetGValue(c42) + GetGValue(c43) + GetGValue(c44)
-        ) / 25;
-    byte b = (GetBValue(c00) + GetBValue(c01) + GetBValue(c02) + GetBValue(c03) + GetBValue(c04)
-        + GetBValue(c10) + GetBValue(c11) + GetBValue(c12) + GetBValue(c13) + GetBValue(c14)
-        + GetBValue(c20) + GetBValue(c21) + GetBValue(c22) + GetBValue(c23) + GetBValue(c24)
-        + GetBValue(c30) + GetBValue(c31) + GetBValue(c32) + GetBValue(c33) + GetBValue(c34)
-        + GetBValue(c40) + GetBValue(c41) + GetBValue(c42) + GetBValue(c43) + GetBValue(c44)
-        ) / 25;
-
-    return RGB(r, g, b);
-}
-
-COLORREF mosaic5x5Filter(HDC hMemDC, int orgX, int orgY) {
+    // 以下、境界処理が必要ですが、省略。
     int x = orgX - orgX % 5;
     int y = orgY - orgY % 5;
-    COLORREF c00 = GetPixel(hMemDC, x, y);
-    COLORREF c01 = GetPixel(hMemDC, x, y + 1);
-    COLORREF c02 = GetPixel(hMemDC, x, y + 2);
-    COLORREF c03 = GetPixel(hMemDC, x, y + 3);
-    COLORREF c04 = GetPixel(hMemDC, x, y + 4);
-    COLORREF c10 = GetPixel(hMemDC, x + 1, y);
-    COLORREF c11 = GetPixel(hMemDC, x + 1, y + 1);
-    COLORREF c12 = GetPixel(hMemDC, x + 1, y + 2);
-    COLORREF c13 = GetPixel(hMemDC, x + 1, y + 3);
-    COLORREF c14 = GetPixel(hMemDC, x + 1, y + 4);
-    COLORREF c20 = GetPixel(hMemDC, x + 2, y);
-    COLORREF c21 = GetPixel(hMemDC, x + 2, y + 1);
-    COLORREF c22 = GetPixel(hMemDC, x + 2, y + 2);
-    COLORREF c23 = GetPixel(hMemDC, x + 2, y + 3);
-    COLORREF c24 = GetPixel(hMemDC, x + 2, y + 4);
-    COLORREF c30 = GetPixel(hMemDC, x + 3, y);
-    COLORREF c31 = GetPixel(hMemDC, x + 3, y + 1);
-    COLORREF c32 = GetPixel(hMemDC, x + 3, y + 2);
-    COLORREF c33 = GetPixel(hMemDC, x + 3, y + 3);
-    COLORREF c34 = GetPixel(hMemDC, x + 3, y + 4);
-    COLORREF c40 = GetPixel(hMemDC, x + 4, y);
-    COLORREF c41 = GetPixel(hMemDC, x + 4, y + 1);
-    COLORREF c42 = GetPixel(hMemDC, x + 4, y + 2);
-    COLORREF c43 = GetPixel(hMemDC, x + 4, y + 3);
-    COLORREF c44 = GetPixel(hMemDC, x + 4, y + 4);
+    DWORD r = mosaic5x5(16, src, x + y * width, width);
+    DWORD g = mosaic5x5(8, src, x + y * width, width);
+    DWORD b = mosaic5x5(0, src, x + y * width, width);
 
-    byte r = (GetRValue(c00) + GetRValue(c01) + GetRValue(c02) + GetRValue(c03) + GetRValue(c04)
-        + GetRValue(c10) + GetRValue(c11) + GetRValue(c12) + GetRValue(c13) + GetRValue(c14)
-        + GetRValue(c20) + GetRValue(c21) + GetRValue(c22) + GetRValue(c23) + GetRValue(c24)
-        + GetRValue(c30) + GetRValue(c31) + GetRValue(c32) + GetRValue(c33) + GetRValue(c34)
-        + GetRValue(c40) + GetRValue(c41) + GetRValue(c42) + GetRValue(c43) + GetRValue(c44)
-        ) / 25;
-    byte g = (GetGValue(c00) + GetGValue(c01) + GetGValue(c02) + GetGValue(c03) + GetGValue(c04)
-        + GetGValue(c10) + GetGValue(c11) + GetGValue(c12) + GetGValue(c13) + GetGValue(c14)
-        + GetGValue(c20) + GetGValue(c21) + GetGValue(c22) + GetGValue(c23) + GetGValue(c24)
-        + GetGValue(c30) + GetGValue(c31) + GetGValue(c32) + GetGValue(c33) + GetGValue(c34)
-        + GetGValue(c40) + GetGValue(c41) + GetGValue(c42) + GetGValue(c43) + GetGValue(c44)
-        ) / 25;
-    byte b = (GetBValue(c00) + GetBValue(c01) + GetBValue(c02) + GetBValue(c03) + GetBValue(c04)
-        + GetBValue(c10) + GetBValue(c11) + GetBValue(c12) + GetBValue(c13) + GetBValue(c14)
-        + GetBValue(c20) + GetBValue(c21) + GetBValue(c22) + GetBValue(c23) + GetBValue(c24)
-        + GetBValue(c30) + GetBValue(c31) + GetBValue(c32) + GetBValue(c33) + GetBValue(c34)
-        + GetBValue(c40) + GetBValue(c41) + GetBValue(c42) + GetBValue(c43) + GetBValue(c44)
-        ) / 25;
-
-    return RGB(r, g, b);
-}
-
-COLORREF grayFilter(HDC hMemDC, int x, int y) {
-    COLORREF col = GetPixel(hMemDC, x, y);
-    //byte gray = (GetRValue(col) + GetGValue(col) + GetBValue(col)) / 3;
-
-    // CIE XYZ 規格
-    byte gray = 0.2126 * GetRValue(col) + 0.7152 * GetGValue(col) + 0.0722 * GetBValue(col);
-
-    return RGB(gray, gray, gray);
+    dst[index] = r << 16 | g << 8 | b;
 }
 
 LRESULT CALLBACK WndProc(
@@ -191,56 +105,77 @@ LRESULT CALLBACK WndProc(
 {
     static HBITMAP  hBitmap;    // ビットマップ
     static HDC      hMemDC;     // オフスクリーン
-    static BITMAP bmp_info = { 0 };
+
+    // 【別解】ビットマップを配列で処理する場合
+    static LPDWORD  lpRGB;
+    static BITMAPINFO bi = { 0 };
+
 
     static HDC hMemFilterDC;
     static HBITMAP hBitmapFilter;
 
-    int FILTER_X = 600;
-    int FILTER_Y = 100;
-    int FILTER_WIDTH = 240;
-    int FILTER_HEIGHT = 240;
+    RECT rectFilter = { 600, 100, 840, 340 };
 
     HDC hdc;
 
+    // benchmark
+    std::chrono::system_clock::time_point start, end;
+    long microsec = 0;
+
     switch (uMsg) {
-    case WM_CREATE:
+    case WM_CREATE: {
+        // ベンチマーク開始（発展課題用）
+        start = std::chrono::system_clock::now();
+
         // オフスクリーンをメモリデバイスコンテキストを用いて作成
         hdc = GetDC(hwnd);
-        hMemDC = CreateCompatibleDC(hdc);
+
+        HDC hMemDCTmp = CreateCompatibleDC(hdc);
         // 画像をファイルから読み込む
-
-        hBitmap = (HBITMAP)LoadImage(NULL, L"kyocotan_tate.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+        HBITMAP hBitmapTmp = (HBITMAP)LoadImage(NULL, L"kyocotan.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
         // ビットマップの情報を取得し、bmp_infoに保管
-        GetObject(hBitmap, (int)sizeof(BITMAP), &bmp_info);
+        BITMAP bmp_info = { 0 };
+        GetObject(hBitmapTmp, (int)sizeof(BITMAP), &bmp_info);
+        SelectObject(hMemDCTmp, hBitmapTmp);
+        
+        hMemDC = CreateCompatibleDC(hdc);
+        bi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bi.bmiHeader.biWidth = bmp_info.bmWidth;
+        bi.bmiHeader.biHeight = bmp_info.bmHeight;
+        bi.bmiHeader.biPlanes = bmp_info.bmPlanes;
+        bi.bmiHeader.biBitCount = bmp_info.bmBitsPixel;
+        bi.bmiHeader.biCompression = BI_RGB;
+        hBitmap = CreateDIBSection(hMemDC, &bi, DIB_RGB_COLORS, (LPVOID*)&lpRGB, NULL, 0);
         SelectObject(hMemDC, hBitmap);
+        // 画像ファイルの読込は手間なのでLoadImageしてBitBltしました。
+        BitBlt(hMemDC, 0, 0, bmp_info.bmWidth, bmp_info.bmHeight, hMemDCTmp, 0, 0, SRCCOPY);
 
-        // フィルタ処理用のオフスクリーン
-        hMemFilterDC = CreateCompatibleDC(hdc);
-        hBitmapFilter = CreateCompatibleBitmap(hdc, FILTER_WIDTH, FILTER_HEIGHT);
-        SelectObject(hMemFilterDC, hBitmapFilter);
-
-        // いろいろなフィルタ処理
-        for (int x = FILTER_X; x < FILTER_X + FILTER_WIDTH; x++) {
-            for (int y = FILTER_Y; y < FILTER_Y + FILTER_HEIGHT; y++) {
-                // 3x3平均
-                //SetPixelV(hMemFilterDC, x - FILTER_X, y - FILTER_Y, avr3x3Filter(hMemDC, x, y));
-
-                // 5x5平均
-                // SetPixelV(hMemFilterDC, x - FILTER_X, y - FILTER_Y, avr5x5Filter(hMemDC, x, y));
-
-                // グレースケール化
-                // SetPixelV(hMemFilterDC, x - FILTER_X, y - FILTER_Y, grayFilter(hMemDC, x, y));
-
-                // 基本課題：5x5のモザイク
-                SetPixelV(hMemFilterDC, x - FILTER_X, y - FILTER_Y, mosaic5x5Filter(hMemDC, x, y));
-            }
-        }
-        BitBlt(hMemDC, FILTER_X, FILTER_Y, FILTER_WIDTH, FILTER_HEIGHT,
-            hMemFilterDC, 0, 0, SRCCOPY);
-
+        DeleteDC(hMemDCTmp);
+        DeleteObject(hBitmapTmp);
         ReleaseDC(hwnd, hdc);
+
+
+        // フィルタ処理用の配列
+        LPDWORD lpRGBFilter = new DWORD[bmp_info.bmWidth * bmp_info.bmHeight];
+
+        // DIBの配列では左下が原点座標なので注意
+        int top = bmp_info.bmHeight - rectFilter.bottom;
+        int bottom = top + (rectFilter.bottom - rectFilter.top);
+        RECT rectDIB = { rectFilter.left, top, rectFilter.right, bottom };
+
+        for (int i = 0; i < bmp_info.bmWidth * bmp_info.bmHeight; i++) {
+            mosaic5x5Filter(lpRGB, lpRGBFilter, rectDIB, i, bmp_info.bmWidth);
+        }
+        memcpy(lpRGB, lpRGBFilter, bmp_info.bmWidth * bmp_info.bmHeight * sizeof(DWORD));
+        delete[] lpRGBFilter;
+
+        // ベンチマーク終了
+        end = std::chrono::system_clock::now();
+        microsec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        OutputDebugString((std::to_wstring(microsec) + L"\n").c_str());
+
         return 0;
+    }
     case WM_CLOSE:
         // オフスクリーンの破棄
         DeleteDC(hMemDC);
@@ -256,8 +191,7 @@ LRESULT CALLBACK WndProc(
         // ビットブロック転送
         RECT rc;
         GetClientRect(hwnd, &rc); // クライアント領域のサイズ
-        
-        /*
+
         BitBlt(
             hdc,
             0,
@@ -268,13 +202,13 @@ LRESULT CALLBACK WndProc(
             0,
             0,
             SRCCOPY);
-        */
-        
+
+        /*
         int width = rc.right - rc.left;
         int height = rc.bottom - rc.top;
 
-        int imgW = bmp_info.bmWidth;
-        int imgH = bmp_info.bmHeight;
+        int imgW = bi.bmiHeader.biWidth;
+        int imgH = bi.bmiHeader.biHeight;
 
         int newImgW = 0;
         int newImgH = 0;
@@ -307,7 +241,7 @@ LRESULT CALLBACK WndProc(
             imgH,
             SRCCOPY
         );
-        
+        */
 
         EndPaint(hwnd, &paint);
 
